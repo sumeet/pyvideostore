375b2ac461ac65fa38b3ae4574c52a6ab108f5a7
Sumeet Agarwal <sumeet.a@gmail.com>
2012-04-01 23:36:52 -0700

    remove dead code

--- movie.py
+++ movie.py
@@ -19,10 +19,7 @@ class ChildrensMovie(object):
         return 1
 
     def amount(self, days_rented):
-        rental_amount = 1.5
-        if days_rented > 3:
-            rental_amount += (days_rented - 3) * 1.5
-        return rental_amount
+        return 1.5
 
 
 class RegularMovie(object):
