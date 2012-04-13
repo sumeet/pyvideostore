6d0e9e89b3b7238bd85d9be6662d36b36f57c9a8
Sumeet Agarwal <sumeet@yelp.com>
2012-04-13 11:56:00 -0700

    clean up tests.

--- video_store_test.py
+++ video_store_test.py
@@ -17,22 +17,19 @@ class VideoStoreTest(unittest.TestCase):
         self.regular_2 = RegularMovie('8 1/2')
         self.regular_3 = RegularMovie('Eraserhead')
 
-    def test_single_new_release_statement(self):
+    def test_single_new_release_statement_totals(self):
         self.statement.add_rental(Rental(self.new_release_1, 3))
-        self.statement.generate()
         expect(self.statement.frequent_renter_points) == 2
         expect(self.statement.amount_owed) == 9
 
-    def test_dual_new_release_statement(self):
+    def test_dual_new_release_statement_totals(self):
         self.statement.add_rental(Rental(self.new_release_1, 3))
         self.statement.add_rental(Rental(self.new_release_2, 3))
-        self.statement.generate()
         expect(self.statement.frequent_renter_points) == 4
         expect(self.statement.amount_owed) == 18
 
-    def test_single_childrens_statement(self):
+    def test_single_childrens_statement_totals(self):
         self.statement.add_rental(Rental(self.childrens, 3))
-        self.statement.generate()
         expect(self.statement.frequent_renter_points) == 1
         expect(self.statement.amount_owed) == 1.5
 
@@ -40,7 +37,6 @@ class VideoStoreTest(unittest.TestCase):
         self.statement.add_rental(Rental(self.regular_1, 1))
         self.statement.add_rental(Rental(self.regular_2, 2))
         self.statement.add_rental(Rental(self.regular_3, 3))
-        self.statement.generate()
         expect(self.statement.frequent_renter_points) == 3
         expect(self.statement.amount_owed) == 7.5
 
@@ -48,13 +44,12 @@ class VideoStoreTest(unittest.TestCase):
         self.statement.add_rental(Rental(self.regular_1, 1))
         self.statement.add_rental(Rental(self.regular_2, 2))
         self.statement.add_rental(Rental(self.regular_3, 3))
-        self.assertEquals(
-            'Rental Record for Fred\n'
-            '\tPlan 9 from Outer Space\t2\n'
-            '\t8 1/2\t2\n'
-            '\tEraserhead\t3.5\n'
-            'You owed 7.5\nYou earned 3 frequent renter points\n',
-            self.statement.generate())
+        statement = ('Rental Record for Fred\n'
+                     '\tPlan 9 from Outer Space\t2\n'
+                     '\t8 1/2\t2\n'
+                     '\tEraserhead\t3.5\n'
+                     'You owed 7.5\nYou earned 3 frequent renter points\n')
+        expect(statement) == self.statement.generate()
 
 
 if __name__ == '__main__':
